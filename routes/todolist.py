27be6d7a4b112aea4bce9d89a7082998aea81571
balbino aylagas <balbino_a@hotmail.com>
2022-03-16 18:56:04 -0600

    add new task

--- routes/todolist.py
+++ routes/todolist.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user, login_required
 from forms.taskCreateForm import TaskCreateForm
+from utils.db import db
 from models.user import User
 from models.task import Task
 
@@ -20,6 +21,7 @@ def home():
     return render_template("todolist/home.html", user=current_user, userList=userList)
 
 
+# http://127.0.0.1:5000/todolist/tasklist/2
 @todolist.route("/tasklist/<int:userId>")
 @login_required
 def tasklist(userId):
@@ -27,10 +29,15 @@ def tasklist(userId):
     return render_template("todolist/tasklist.html", user=current_user, userId=userId, tasks=currentTaskList)
 
 
+# http://127.0.0.1:5000/todolist/tasklist/create/2
 @todolist.route("/tasklist/create/<int:userId>", methods=["GET", "POST"])
 @login_required
 def create(userId):
     form = TaskCreateForm()
     if form.validate_on_submit():
-        pass
+        description = form.description.data
+        newTask = Task(userId, description)
+        db.session.add(newTask)
+        db.session.commit()
+        return redirect(url_for("todolist.tasklist", userId=userId))
     return render_template("todolist/create.html", form=form, user=current_user, userId=userId)
